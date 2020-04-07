3b4cd5e0742596970b913ee12ef537c04debc4e7
Caio Mendes <caiosamarone10@hotmail.com>
2020-04-07 14:31:35 -0300

    Inicio validacao Campos

--- src/main/java/com/br/prjmvc/domain/Departamento.java
+++ src/main/java/com/br/prjmvc/domain/Departamento.java
@@ -1,10 +1,13 @@
 package com.br.prjmvc.domain;
 
-import javax.persistence.Entity;
-
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @SuppressWarnings("serial")
 @Entity
@@ -13,6 +16,9 @@ public class Departamento extends AbstractEntity<Long> {
 	//O id dessa classe vai ser herdado do tipo Long
 	//Não precisa instanciar o id, pq ele ja vai ser herdado pelo abstractEntity
 	
+	
+	@NotBlank(message = "Informe um nome.")
+	@Size(min = 3, max = 60, message = "O nome do departamento deve ter entre {min} e {max} caracteres")
 	//não é nulo, é unica, e o tamanho da coluna
 	@Column(name = "nome", nullable = false, unique = true, length = 60)
 	private String nome;
